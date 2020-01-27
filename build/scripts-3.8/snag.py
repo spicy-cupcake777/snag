efdd1ec24889d296fff889ae77f28956b949bf4c
= <danielscottgentry@gmail.com>
2020-01-27 17:58:33 -0600

    Fixed dependencies

--- build/scripts-3.8/snag.py
+++ build/scripts-3.8/snag.py
@@ -1,4 +1,4 @@
-#!python
+#!/data/data/com.termux/files/usr/bin/python
 
 from requests import get
 from bottle import *
--- setup.py
+++ setup.py
@@ -15,6 +15,10 @@ setuptools.setup(
         long_description=long_description,
         long_description_content_type='text/markdown',
         packages=setuptools.find_packages(),
+        install_requires= [
+            'requests',
+            'bottle'
+        ],
         classifiers=[
             'Programming Language :: Python :: 3',
             'License :: OSI Approved :: GPL',
