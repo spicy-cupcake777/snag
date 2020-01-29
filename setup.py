847d874898112f7792cf5f2aee616706bd74aee6
= <danielscottgentry@gmail.com>
2020-01-28 20:54:45 -0600

    Increment the version number so's I can upload it to pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
         name='snag-dom',
-        version='0.9',
+        version='0.10',
         author='Cassandra Gentry',
         author_email='cassiegentlekitty@gmail.com',
         description='An HTML utility package',
